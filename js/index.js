c6c8d3c418353c35614917ebb7700e4a23fd83c2
Lewis <lewis.kagia@student.moringaschool.com>
2023-06-27 11:16:15 +0300

    git hub search app

---
+++ js/index.js
@@ -0,0 +1,31 @@
+const githubform = document.getElementById('githubform');
+const search = document.getElementById('search');
+const githubcontainer = document.getElementById('githubcontainer');
+const userlist = document.getElementById('userlist');
+
+//let username = typicode;
+fetch (`https://api.github.com/search/users?q=${username}`)
+Method : "POST"
+Accept : application/vnd.github.v3+json
+.then(res => res.db.json())
+.then(data => {
+    displaygithubcontainer(data.items);
+})
+
+function displaygithubcontainer(users){
+    githubcontainer.innerText = ' ';
+}
+
+githubform.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const username=search.value;
+})
+
+userDiv.addEventListener('click'), function() {
+    fetch(`https://api.github.com/search/users?q=${username}`)
+    .then(res => res.db.json())
+    .then(data => {
+        displayuserlist(data);
+    })
+    
+}
